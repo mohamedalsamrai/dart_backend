aa4f56110e2fc8a189c729e34b45a7f9d353edd3
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2025-01-29 14:42:25 +0300

    Refactor server structure by moving route handling to a dedicated Routes class and simplifying main server function

---
+++ lib/routes/routes.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:my_server/server_info.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_static/shelf_static.dart';
+
+class Routes {
+  static final router = Router();
+  static final indexRoute = createStaticHandler(
+    'public',
+    defaultDocument: 'index.html',
+  );
+
+  static Future<void> call() async {
+    router.get('/users', (Request request) async {
+      final dbConnection = await ServerInfo.connectToDatabase();
+      try {
+        final results =
+            await dbConnection.query('SELECT id, name, email FROM user');
+        final users = results
+            .map((row) => {'id': row[0], 'name': row[1], 'email': row[2]})
+            .toList();
+
+        return Response.ok(jsonEncode(users),
+            headers: {'Content-Type': 'application/json'});
+      } catch (e) {
+        return Response.internalServerError(body: 'Error: $e');
+      } finally {
+        await dbConnection.close();
+      }
+    });
+
+    router.post('/add-user', (Request request) async {
+      final payload = await request.readAsString();
+      final data = jsonDecode(payload) as Map<String, dynamic>;
+
+      final name = data['name'];
+      final email = data['email'];
+      final password = data['password'];
+
+      if (name == null || email == null || password == null) {
+        return Response.badRequest(body: 'الاسم أو البريد الإلكتروني مفقود.');
+      }
+
+      final dbConnection = await ServerInfo.connectToDatabase();
+      try {
+        await dbConnection.query(
+          'INSERT INTO user (name, email, password) VALUES (?, ?, ?)',
+          [name, email, password],
+        );
+
+        return Response.ok('تم إضافة المستخدم بنجاح.');
+      } catch (e) {
+        return Response.internalServerError(body: 'Error: $e');
+      } finally {
+        await dbConnection.close();
+      }
+    });
+  }
+
+ 
+}
--- lib/server_info.dart
+++ lib/server_info.dart
@@ -1,5 +1,8 @@
 import 'package:dotenv/dotenv.dart';
+import 'package:my_server/routes/routes.dart';
 import 'package:mysql1/mysql1.dart';
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf/shelf.dart';
 
 class Pram {
   Pram(this.name) : value = name;
@@ -17,25 +20,32 @@ class Pram {
 }
 
 class ServerInfo {
-  static final env = DotEnv(includePlatformEnvironment: true)..load();
-  static final sqlPort = env["Sql_Port"];
-  static final host = env["Host"];
-  static final user = env["User"];
-  static final password = env["Password"];
-  static final database = env["Database"];
-  static final serverPort = env["Server_Port"];
+  static final _env = DotEnv(includePlatformEnvironment: true)..load();
+  static final _sqlPort = _env["Sql_Port"];
+  static final _host = _env["Host"];
+  static final _user = _env["User"];
+  static final _password = _env["Password"];
+  static final _database = _env["Database"];
+  static final _serverPort = _env["Server_Port"];
 
   static Future<MySqlConnection> connectToDatabase() async {
     final settings = ConnectionSettings(
-      host: host!,
-      port: int.parse(sqlPort!),
-      user: user,
-      password: password,
-      db: database,
+      host: _host!,
+      port: int.parse(_sqlPort!),
+      user: _user,
+      password: _password,
+      db: _database,
     );
     return await MySqlConnection.connect(settings);
   }
+ static Future<void> runServer() async {
+    Routes.call();
+    final handler = Cascade().add(Routes.indexRoute).add(Routes.router).handler;
 
+    final server =
+        await io.serve(handler, _host!, int.parse(_serverPort!));
+    print('🚀 Server running on http://${server.address.host}:${server.port}');
+  }
   static Future<void> createTable(String tableName, List<String> param) async {
     final db = await connectToDatabase();
     try {
